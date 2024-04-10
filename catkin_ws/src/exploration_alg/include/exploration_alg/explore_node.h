ab00b7aa5e949bab0f6e01aa6510c9279fe763ab
Richard Zhao <zhao.richa@northeastern.edu>
2024-04-10 02:18:28 +0000

    partially implemented exploration code. need to test. commit just to show update

--- catkin_ws/src/exploration_alg/include/exploration_alg/explore_node.h
+++ catkin_ws/src/exploration_alg/include/exploration_alg/explore_node.h
@@ -1,13 +1,12 @@
 #pragma once
+
 #include "ros/ros.h"
 
 /**
  * A ROS node that handles exploration. This will run frontier detection and select an exploration goal.
+ * Missing implementation.
  */
 class ExploreNode {
    private:
     ros::NodeHandle node;
-
-   public:
-    ExploreNode();
 };
--- catkin_ws/src/exploration_alg/src/explore_node.cpp
+++ catkin_ws/src/exploration_alg/src/explore_node.cpp
@@ -2,10 +2,8 @@
 
 #include "ros/ros.h"
 
-ExploreNode::ExploreNode() {}
-
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "explore_node");
+    ros::init(argc, argv, "hi");
     ExploreNode explorenode;
     ros::spin();
     return 0;
